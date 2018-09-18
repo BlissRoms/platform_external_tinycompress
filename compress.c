6ebb3e79e7438ac9af77ff41a47c40177d55ef3b
Jackeagle <jackeagle102@gmail.com>
2018-09-18 07:57:14 -0400

    tinycompress: Disable vendor ext flag
    Change-Id: Ibd3db62ef47ab835680b8278d20bc8c26651c62b
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- compress.c
+++ compress.c
@@ -572,7 +572,7 @@ int compress_set_gapless_metadata(struct compress *compress,
 	return 0;
 }
 
-#ifdef USE_VENDOR_EXTN
+//#ifdef USE_VENDOR_EXTN
 #if defined(SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM)
 int compress_set_next_track_param(struct compress *compress,
 	union snd_codec_options *codec_options)
@@ -591,7 +591,7 @@ int compress_set_next_track_param(struct compress *compress __unused,
 	return 0;
 }
 #endif
-#endif
+//#endif
 
 bool is_codec_supported(unsigned int card, unsigned int device,
 		unsigned int flags, struct snd_codec *codec)
@@ -651,7 +651,7 @@ int compress_wait(struct compress *compress, int timeout_ms)
 	return oops(compress, EIO, "poll signalled unhandled event");
 }
 
-#ifdef USE_VENDOR_EXTN
+//#ifdef USE_VENDOR_EXTN
 int compress_get_metadata(struct compress *compress,
 		struct snd_compr_metadata *mdata) {
 	int version;
@@ -684,4 +684,4 @@ int compress_set_metadata(struct compress *compress,
 	}
 	return 0;
 }
-#endif
+//#endif
